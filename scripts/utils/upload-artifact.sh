3bd184749b09c0213f4dd33516598b5111520f04
sri raghavan <sri@withorb.com>
2026-07-07 20:13:32 -0400

    Build SDK
    Stainless-Generated-From: 62a9a14b602bf3a86b2f713795450a2bb8ac40f7

--- scripts/utils/upload-artifact.sh
+++ scripts/utils/upload-artifact.sh
@@ -43,7 +43,7 @@ UPLOAD_RESPONSE=$(curl -v -X PUT \
 
 if echo "$UPLOAD_RESPONSE" | grep -q "HTTP/[0-9.]* 200"; then
   echo -e "\033[32mUploaded build to Stainless storage.\033[0m"
-  echo -e "\033[32mInstallation: Download and unzip: 'https://pkg.stainless.com/s/orb-go/$SHA'. Run 'go mod edit -replace github.com/orbcorp/orb-go=/path/to/unzipped_directory'.\033[0m"
+  echo -e "\033[32mInstallation: Download and unzip: 'https://pkg.stainless.com/s/orb-go-staging/$SHA'. Run 'go mod edit -replace github.com/orbcorp/orb-go=/path/to/unzipped_directory'.\033[0m"
 else
   echo -e "\033[31mFailed to upload artifact.\033[0m"
   exit 1
